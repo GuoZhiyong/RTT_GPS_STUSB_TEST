2ff59b08ddb5d4be1ae6828185392cf2db84e3b6
Zhiyong Guo <bitterguo.tcb@gmail.com>
2013-04-19 08:17:46 +0800

    normal commit

---
+++ bsp/stm32f40x_production/drivers/sle4442.h
@@ -0,0 +1,30 @@
+/*SLE4442 初始化硬件*/
+#ifndef _SLE4442_H_
+#define _SLE4442_H_
+
+#include <rtthread.h> 
+#include <rthw.h>
+#include "stm32f4xx.h"
+#include "usart.h"
+#include "board.h"
+#include <serial.h>
+
+#include  <stdlib.h>//数字转换成字符串
+#include  <stdio.h>
+#include  <string.h>
+#include  <ctype.h>
+#include  <stdlib.h>
+#include  <stdarg.h>
+
+
+#define  C_50Ms     1638 //50ms时钟中断
+
+#define  TRUE   1
+#define  FALSE  0
+
+
+void Init_4442(void);
+void CheckICCard(void);
+
+#endif
+
